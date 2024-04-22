e1fb9c33b988ec1079618b042a886fff99aa0caa
lewis <lewispower061@mail.com>
2024-04-22 20:44:32 +0200

    immaKMS

--- src/MorseEncoder.java
+++ src/MorseEncoder.java
@@ -1,5 +1,5 @@
 import java.beans.Encoder;
-
+/*
 public class MorseEncoder {
     BinaryTree <String> letterTree;
     BinaryTree <String> currentTree;
@@ -54,3 +54,4 @@ public class MorseEncoder {
         System.out.println(finishedSentence + " = " + encodedMsg);
     }
 }
+*/
