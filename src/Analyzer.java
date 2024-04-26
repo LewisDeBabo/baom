0f07b8906909d2cd1e4c56d74b42c652356de05b
lewis.loughlin <lewispower061@gmail.com>
2024-04-26 13:18:41 +0200

    immakms

---
+++ src/Analyzer.java
@@ -0,0 +1,39 @@
+
+
+public class Analyzer {
+
+    public <T> int gibTiefe(BinaryTree<T> binaryTree){
+        if(null == binaryTree || binaryTree.isEmpty())
+            return -1;
+        int l, r;
+        if (!binaryTree.isEmpty()) {
+            l = gibTiefe(binaryTree.getLeftTree());
+            r = gibTiefe(binaryTree.getRightTree());
+            if (l > r){
+                return 1 + l;
+            } else {
+                return 1 + r;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    public <T> int gibGesamtKnotenAnzahl(BinaryTree<T> binaryTree){
+        if(null == binaryTree || binaryTree.isEmpty())
+            return 0;
+        return 1 + gibGesamtKnotenAnzahl(binaryTree.getLeftTree()) + gibGesamtKnotenAnzahl(binaryTree.getRightTree());
+    }
+
+
+
+    public static void main(String[] args) {
+        Analyzer analyzer = new Analyzer();
+        BinaryTree <Integer> binaryTree = new BinaryTree<Integer>(0, new BinaryTree<Integer>(1, new BinaryTree<Integer>(3), new BinaryTree<Integer>(5)), new BinaryTree<Integer>(2, new BinaryTree<Integer>(4), new BinaryTree<Integer>(6)));
+        System.out.println("Tiefe => " + analyzer.gibTiefe(binaryTree));
+        System.out.println(analyzer.gibGesamtKnotenAnzahl(binaryTree));
+        System.out.println(analyzer.gibAnzahlDerBlätter(binaryTree));
+        System.out.println(analyzer.gibSummeAllerKnoten(binaryTree));
+        System.out.println(analyzer.gibMaximumDesBaumes(binaryTree));
+
+}
