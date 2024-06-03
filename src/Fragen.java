027069e4d8cdd7cfaaf39ada01260d9d11d1cea6
lewis.loughlin <lewispower061@gmail.com>
2024-06-03 13:23:35 +0200

    iuuiuiff

---
+++ src/Fragen.java
@@ -0,0 +1,8 @@
+import jdk.jfr.ContentType;
+
+public class Fragen {
+    public String getFrage(ContentType frage){
+
+    }
+
+}
---
+++ src/Sklave.java
@@ -0,0 +1,7 @@
+public class Sklave {
+    Waffe waffe;
+    Fragen fragen;
+
+
+
+}
