92df7cc3e7d7023469edce1ae1ab2ce74ed7a937
lewis.loughlin <lewispower061@gmail.com>
2024-06-10 13:08:40 +0200

    iuuiuiff

--- src/SpielGUI.java
+++ src/SpielGUI.java
@@ -1,10 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SpielGUI {
     private JButton jaButton;
     private JButton neinButton;
     private JTextArea Frage;
 
+
+    public SpielGUI() {
+        jaButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        neinButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 
     private void createUIComponents() {
 
