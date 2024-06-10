9846b88d8b9c333c6db01a9f342b6b9d09cc1d5b
lewis.loughlin <lewispower061@gmail.com>
2024-06-10 13:27:21 +0200

    iuuiuiff

--- src/Waffenbaum.java
+++ src/Waffenbaum.java
@@ -1,8 +1,10 @@
 public class Waffenbaum {
 
     public SpielGUI gui;
-
     public BinaryTree<Bauminhalt> baum;
+    public BinaryTree<Bauminhalt> zeiger;
+
+    String zwischenspeicher;
 
     public Waffenbaum(){
         gui = new SpielGUI();
@@ -11,20 +13,25 @@ public class Waffenbaum {
         Waffe w1 = new Waffe("AK-47");
         Waffe w2 = new Waffe("Glock 19");
 
-        new BinaryTree<Bauminhalt>(f1,new BinaryTree<Bauminhalt>(w1,null,null),new BinaryTree<Bauminhalt>(w2,null,null));
+        baum = new BinaryTree<Bauminhalt>(f1,new BinaryTree<Bauminhalt>(w1,null,null),new BinaryTree<Bauminhalt>(w2,null,null));
+        zeiger = baum;
     }
 
     public void ersetzen(){
 
     }
 
-    /*
+    public void ja(){
+        zeiger = zeiger.getLeftTree();
+    }
+
+    public void nein(){
+        zeiger = zeiger.getRightTree();
+    }
+
     public void hinzufuegen(){
 
     }
-    */
-
-
 
     //fehlt?
     public boolean istEsEmpirischNichtexistentFragezeichen(){
